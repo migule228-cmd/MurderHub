b832e2de10cfb4446e3fc901a078fb1948485ff1
migule228-cmd <migule228@gmail.com>
2026-03-08 18:36:04 +0000

    Fix syntax error in game ID comparison

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,6 @@
 while true do
   if game.id = 142823291 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/migule228-cmd/MurderHub/blob/main/Main.lua")()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/migule228-cmd/MurderHub/blob/main/Main.lua"))()
   end
   end
 
