15923dfe6291ae7686d0f01a495600d53944ce1a
migule228-cmd <migule228@gmail.com>
2026-03-08 18:33:42 +0000

    Fix syntax error in Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,4 @@
 if game.id = 142823291 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/migule228-cmd/MurderHub/refs/heads/main/Main.lua")
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/migule228-cmd/MurderHub/refs/heads/main/Main.lua")()
   end
 
