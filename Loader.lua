4d47b534ec419c6da9e270ba3ad0115c16da82d9
migule228-cmd <migule228@gmail.com>
2026-03-08 18:35:41 +0000

    Fix game ID check and update loadstring URL

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,6 @@
-if game.id = 142823291 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/migule228-cmd/MurderHub/refs/heads/main/Main.lua")()
+while true do
+  if game.id = 142823291 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/migule228-cmd/MurderHub/blob/main/Main.lua")()
+  end
   end
 
